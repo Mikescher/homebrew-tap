b866d3e888418b51263923454854f3def55cbdfd
Mike Schwörer <pubgit@mikescher.com>
2022-11-16 12:10:09 +0100

    ffsclient v1.3.0

--- ffsclient.rb
+++ ffsclient.rb
@@ -2,8 +2,8 @@ class Ffsclient < Formula
 
   desc     " A cli for firefox-sync (firefox bookmarks, passwords, account, ...) "
   homepage "https://github.com/Mikescher/firefox-sync-client"
-  url      "https://github.com/Mikescher/firefox-sync-client/releases/download/v1.2.0/ffsclient_macos-amd64"
-  sha256   "98114ec9c3e2e9eb4b2bfd47caf1288c4d5216f8155b4260d85fb7d2783f7010"
+  url      "https://github.com/Mikescher/firefox-sync-client/releases/download/v1.3.0/ffsclient_macos-amd64"
+  sha256   "ae0584405abc39df07d75c3a8e43a5d21bd17797705fb59f95f296b57d113c43"
 
   def install
     bin.install "ffsclient_macos-amd64" => "ffsclient"
