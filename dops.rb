e58fd070972b5f9392f7ac84b34fd02ee7d362d3
Mike Schwörer <pubgit@mikescher.com>
2023-01-12 19:40:05 +0100

    dops v1.7

--- dops.rb
+++ dops.rb
@@ -2,8 +2,8 @@ class Dops < Formula
 
   desc     " A replacement for the default docker-ps that tries really hard to fit into the width of your terminal. "
   homepage "https://github.com/Mikescher/better-docker-ps"
-  url      "https://github.com/Mikescher/better-docker-ps/releases/download/v1.6/dops_macos-amd64"
-  sha256   "7527205ba8c156639615d23bcfd17b5b73d6ded93480cdba08987d03883d9c41"
+  url      "https://github.com/Mikescher/better-docker-ps/releases/download/v1.7/dops_macos-amd64"
+  sha256   "a35d8fe71806444bd67266a320bd6294bc40c88231b0460200288f69f2dfb699"
 
   def install
     bin.install "dops_macos-amd64" => "dops"
